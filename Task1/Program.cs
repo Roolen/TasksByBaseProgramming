4f3dfc0ee4254423a62504d3e03618b5fd8edfa0
Artem <zhimovskij@yandex.ru>
2019-01-16 22:21:16 +0500

    Dev: add task 1 on C#

---
+++ Task1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver Oleg = new Driver();
+
+            Oleg.OutputDayViolation();
+        }
+    }
+
+    class Driver
+    {
+        private static bool[] days = new bool[30];
+        private Random rand = new Random();
+        private int dayFirstViolation;
+
+        public Driver()
+        {
+            for (int i = 0; i < days.Length; i++)
+            {
+                days[i] = Convert.ToBoolean(rand.Next(0, 2));
+            }
+
+            DefenitionViolations();
+        }
+
+        private int DefenitionViolations()
+        {
+            
+            for (int i = 0; i < days.Length; i++)
+            {
+                if (days[i])
+                {
+                    dayFirstViolation = i + 1;
+                    break;
+                }
+            }
+
+            return dayFirstViolation;
+        }
+
+        public void OutputDayViolation()
+        {
+            Console.WriteLine(dayFirstViolation > 0 ? $"{dayFirstViolation} Day" : "No violations");
+        }
+    }
+}
