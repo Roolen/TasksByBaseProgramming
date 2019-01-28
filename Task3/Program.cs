b5b32f8dc23ff248a9399b7f8de5d584b1f5e1d4
Artem <zhimovskij@yandex.ru>
2019-01-28 21:55:08 +0500

    Dev: add task 3 on C#

---
+++ Task3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        private static Sample[] samples = new Sample[20];
+        private static Random random = new Random();
+
+        static Program()
+        {
+            for (int i = 0; i < samples.Length; i++)
+            {
+                samples[i] = new Sample();
+            }
+        }
+
+        static void Main()
+        {
+            InternalSamples();
+
+            Console.WriteLine("Specify which item to sort by:\n" +
+                              "1. Lithium\n" +
+                              "2. Cobalt\n" +
+                              "3. Nickel\n" +
+                              "4. Iron\n" +
+                              "5. Gold\n" +
+                              "6. Mercury\n\n");
+
+            OutputSamples((TypeChimicalElement)Convert.ToInt32(Console.ReadLine())-1);
+        }
+
+        static void InternalSamples()
+        {
+            for (int i = 0; i < samples.Length; i++)
+            {
+                samples[i].number = i;
+
+                int remainder = 100;
+                for (int j = 0; j < 6; j++)
+                {
+                    samples[i].chimicalElements[j].Name = ((TypeChimicalElement)j).ToString();
+
+                    int procent = random.Next(0, remainder);
+                    samples[i].chimicalElements[j].Procent = procent;
+
+                    remainder -= procent;
+                }
+            }
+        }
+
+        static void OutputHeader(TypeChimicalElement type)
+        {
+            Console.Write("# sample");
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (i == (int) type)
+                {
+                    var defCol = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("\t" + (TypeChimicalElement)i);
+                    Console.ForegroundColor = defCol;
+                }
+                else
+                {
+                    Console.Write("\t" + (TypeChimicalElement)i);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void OutputSamples(TypeChimicalElement typeChimical)
+        {
+            OutputHeader(typeChimical);
+
+            var samplesTemp = samples.ToList().OrderByDescending(s => s.chimicalElements[(int) typeChimical].Procent);
+
+            foreach (var sample in samplesTemp)
+            {
+                Console.Write(sample.number + "\t");
+
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.Write($"\t{sample.chimicalElements[j].Procent}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Task3/Sample.cs
@@ -0,0 +1,43 @@
+﻿namespace Task3
+{
+    enum TypeChimicalElement
+    {
+        Lithium,
+        Cobalt,
+        Nickel,
+        Iron,
+        Gold,
+        Mercury
+    }
+
+    class Sample
+    {
+        public int number;
+        public ChimicalElement[] chimicalElements = new ChimicalElement[6];
+
+        public Sample()
+        {
+            for (int i = 0; i < chimicalElements.Length; i++)
+            {
+                chimicalElements[i].Name = "";
+                chimicalElements[i].Procent = 0;
+            }
+        }
+
+        public struct ChimicalElement
+        {
+            public string Name { get; set; }
+            private int procent;
+
+            public int Procent
+            {
+                set
+                {
+                    if (value >= 0 && value <= 100)
+                        procent = value;
+                }
+                get => procent;
+            }
+        }
+    }
+}
