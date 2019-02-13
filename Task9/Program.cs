e163db7d3a23761b579dfdc7ca9bfcce1e202a54
Artem <zhimovskij@yandex.ru>
2019-02-13 16:02:47 +0500

    Dev: add task 9 on C#

---
+++ Task9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите количество чисел: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Fibonachi(i) + " ");
+            }
+        }
+
+        private static int Fibonachi(int n) => (n >= 2) ? Fibonachi(n - 1) + Fibonachi(n - 2) : (n == 1) ? 1 : 0;
+    }
+}
