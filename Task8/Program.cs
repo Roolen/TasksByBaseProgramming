66d76f866b132308216a050e2a4ea039aec96c7a
Artem <zhimovskij@yandex.ru>
2019-02-13 12:50:57 +0500

    Dev: add task 8 on C#

---
+++ Task8/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Task8
+{
+    class Program
+    {
+        private static FileInfo filePath = new FileInfo("File.txt");
+
+        static void Main(string[] args)
+        {
+            WriteFile(ReadFile().OrderByDescending(s => s.school).ToList());
+        }
+
+        static List<Student> ReadFile()
+        {
+            List<Student> students = new List<Student>();
+
+            try
+            {
+                using (StreamReader file = new StreamReader(filePath.Name))
+                {
+                    string line;
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        string[] elementsLine = line.Split(new char[] {' '}, StringSplitOptions.None);
+
+                        students.Add(new Student(elementsLine[0], elementsLine[1], elementsLine[2],
+                            Convert.ToInt32(elementsLine[3]), Convert.ToInt32(elementsLine[4])));
+                    }
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+
+            return students;
+        }
+
+        static void WriteFile(List<Student> students)
+        {
+            try
+            {
+
+                using (StreamWriter fileStream = new StreamWriter(File.Create(Path.Combine(filePath.DirectoryName, "Output.txt"))))
+                {
+                    List<List<Student>> schools = new List<List<Student>>();
+                    for (int i = 0, s = 0, j = -1; i < students.Count; i++)
+                    {
+                        if (students[i].school != s)
+                        {
+                            s = students[i].school;
+                            schools.Add(new List<Student>());
+                            j++;
+                        }
+                        schools[j].Add(students[i]);
+                    }
+
+                    for (int i = 0; i < schools.Count; i++)
+                    {
+                        schools[i] = schools[i].OrderByDescending(s => s.score).ToList();
+                    }
+
+                    for (int i = 0; i < schools.Count; i++)
+                    {
+                        fileStream.WriteLine($"Школа №{schools[i].First().school}");
+                        for (int j = 0; j < schools[i].Count; j++)
+                        {
+                            fileStream.WriteLine(schools[i][j].secondName);
+                        }
+                    }
+
+                    Console.WriteLine("Преобразование файла прошло успешно");
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+
+        static void OutputFileInConsole(List<Student> students)
+        {
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.secondName);
+            }
+        }
+    }
+}
---
+++ Task8/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Task8
+{
+    class Student
+    {
+        public readonly string name;
+        public readonly string secondName;
+        public readonly string othestvo;
+        public readonly int school;
+        public readonly int score;
+
+        public Student(string secondName, string name, string othestvo, int school, int score)
+        {
+            this.name = name;
+            this.secondName = secondName;
+            this.othestvo = othestvo;
+            this.school = school;
+            this.score = score;
+        }
+    }
+}
